7e40d194c158d6a2efb035cfaa16660c26bd7f08
N3wSk1Y <miner.fedenko@gmail.com>
2022-01-19 22:49:41 +0300

    v. 2.0.0

--- index.js
+++ index.js
@@ -1,213 +1,193 @@
-const { v4: uuidv4 } = require('uuid');
-const app = require('express')();
-const mysql = require('mysql');
-const bodyParser = require("body-parser");
-const config = require('./config.json')
-const connection = mysql.createConnection({
-  host     : config.DB.host,
-  user     : config.DB.user,
-  password : config.DB.password,
-  database : config.DB.database
+var uuidv4 = require('uuid').v4;
+var app = require('express')();
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var config = require('./config.json');
+var connection = mysql.createConnection({
+    host: config.DB.host,
+    user: config.DB.user,
+    password: config.DB.password,
+    database: config.DB.database
 });
-
-const host = '127.0.0.1';
-const port = 5000;
-
-connection.connect(function(err) {
-    if (err) throw err;
+var host = '127.0.0.1';
+var port = 5000;
+connection.connect(function (err) {
+    if (err)
+        throw err;
     console.log("Connected!");
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Регистрация пользователя
-app.post('/reg', (req, res) => {
-    const username = req.query.name
-    const password = req.query.password
-    const sql = "INSERT INTO `users` (`id`, `name`, `password`) VALUES " + `(Null, '${username}', '${password}')`;
-    connection.query(sql, function (err) {
-    if (err) {
-        switch(err.errno){
-            case 1062: {
-                res.status(409); 
-                res.send('Имя пользователя уже занято'); 
-            }
-            default: {
-                res.status(400);
-                res.send(err);
-            }
+// Регистрация нового пользователя
+app.post('/user', function (req, res) {
+    try {
+        var username_1 = req.query.name;
+        var password = req.query.password;
+        if (!(username_1 && password)) {
+            res.status(400);
+            res.send('Отсутствуют аргументы');
+            return;
         }
-        console.log(err);
-    } else res.status(201); res.send(`Пользователь ${username} был зарегистрирован!`); 
-    });
-})
-
-// Вход в аккаунт (создание сессии)
-app.post('/auth', (req, res) => {
-    const username = req.query.name
-    const password = req.query.password
-    var sql = "SELECT * FROM `users` " + `WHERE name = '${username}' AND password = '${password}'`;
-    connection.query(sql, function (err, result) {
-    if (err) {
-        switch(err.errno) {
-            default: {
-                res.status(400);
-                res.send(err);
-            }
-        }
-    } else if (result.length == 0) {
-        res.status(409);
-        res.send('Аккаунт/Пароль неверный');
+        var sql = "INSERT INTO `users` (`id`, `name`, `password`) VALUES " + "(Null, '".concat(username_1, "', '").concat(password, "')");
+        connection.query(sql, function (err) {
+            res.status(201);
+            res.send("\u041F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C ".concat(username_1, " \u0431\u044B\u043B \u0437\u0430\u0440\u0435\u0433\u0438\u0441\u0442\u0440\u0438\u0440\u043E\u0432\u0430\u043D!"));
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
+// Авторизация (создание сессии)
+app.post('/auth', function (req, res) {
+    var username = req.query.name;
+    var password = req.query.password;
+    if (!(username && password)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
         return;
     }
-    const sessionkey = uuidv4();
-    var sql = `UPDATE \`users\` SET \`sessionkey\` = '${sessionkey}' WHERE \`users\`.\`name\` = '${username}'`
-    connection.query(sql, function (err, result) {
-        if (err) {
-            res.status(400);
-            res.send(err);
-        } else {
-            res.status(200);
-            res.send(`Авторизовано! Ключ сессии: ${sessionkey}`); 
-        }
-    })
-})
-})
-
-// Написание поста
-app.post('/post', (req, res) => {
-    const sessionkey = req.query.sessionkey
-    const content = req.query.content
-    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
-    connection.query(sql, function (err, result) {
-        if (err) {
-            switch (err) {
-                default: {
-                    res.status(400);
-                    res.send(err);
-                }
-            }
-        } else {
-            if (!result.length == 0) {
-            var user = result[0].name;
-            var sql = "INSERT INTO `posts` (`id`, `author`, `content`) VALUES " + `(Null, '${user}', '${content}')`;
-            connection.query(sql, function (err) {
-            if (err) {
-                switch(err.errno) {
-                    default: {
-                        res.status(400);
-                        res.send(err);
-                    }
-                }
-            } else {
-                res.status(201); res.send(`Сообщение ${content} от пользователя ${user} отправлено!`); return;
-            }
-            });
-        } else {
-            res.status(400); res.send('Неверный ключ сессии!'); 
-        }}
-    })
-})
-
-// Получение постов
-app.get('/post', (req, res) => {
-    const sessionkey = req.query.sessionkey;
-    const ID = req.query.ID;
-    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
-    connection.query(sql, function (err, result) {
-        if (err) {
-            switch (err) {
-                default: {
-                    res.status(400);
-                    res.send(err);
-                }
-            }
-        } else {
-            if (!result.length == 0) {
-
-        if (!ID) {
-            var sql = "SELECT * FROM `posts`"
-        } else {
-            var sql = "SELECT * FROM `posts` WHERE `id` = " + `'${ID}'`
-        }
+    try {
+        var sql = "SELECT * FROM `users` " + "WHERE name = '".concat(username, "' AND password = '").concat(password, "'");
         connection.query(sql, function (err, result) {
-            if (err) {
-                switch(err.errno) {
-                    default: {
-                        res.status(400);
-                        res.send(err);
-                    }
-                }
-            } else {
-                res.status(200);
-                if (result.length == 0) {
-                    res.send(`Поста под ID: ${ID} не существует!`)
-                } else res.send(result);
+            if (result.length == 0) {
+                res.status(400);
+                res.send('Аккаунт/Пароль неверный');
+                return;
             }
-        })
-    }}
-    })
-})
-
+            var sessionkey = uuidv4();
+            var sql = "UPDATE `users` SET `sessionkey` = '".concat(sessionkey, "' WHERE `users`.`name` = '").concat(username, "'");
+            connection.query(sql, function (err, result) {
+                res.status(200);
+                res.send("\u0410\u0432\u0442\u043E\u0440\u0438\u0437\u043E\u0432\u0430\u043D\u043E! \u041A\u043B\u044E\u0447 \u0441\u0435\u0441\u0441\u0438\u0438: ".concat(sessionkey));
+            });
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
+// Написание поста
+app.post('/post', function (req, res) {
+    var sessionkey = req.query.sessionkey;
+    var content = req.query.content;
+    if (!(sessionkey && content)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+        var sql = "SELECT * FROM `users` " + "WHERE sessionkey = '".concat(sessionkey, "'");
+        connection.query(sql, function (err, result) {
+            if (!(result.length == 0)) {
+                var user = result[0].name;
+                var sql = "INSERT INTO `posts` (`id`, `author`, `content`) VALUES " + "(Null, '".concat(user, "', '").concat(content, "')");
+                connection.query(sql, function (err) {
+                    res.status(201);
+                    res.send("\u0421\u043E\u043E\u0431\u0449\u0435\u043D\u0438\u0435 ".concat(content, " \u043E\u0442 \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044F ").concat(user, " \u043E\u0442\u043F\u0440\u0430\u0432\u043B\u0435\u043D\u043E!"));
+                    return;
+                });
+            }
+            else
+                res.status(400);
+            res.send('Неверный ключ сессии!');
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
+// Получение всех постов
+app.get('/posts', function (req, res) {
+    var sessionkey = req.query.sessionkey;
+    if (!(sessionkey)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+        var sql = "SELECT * FROM `users` " + "WHERE sessionkey = '".concat(sessionkey, "'");
+        connection.query(sql, function (err, result) {
+            if (!(result.length == 0)) {
+                var sql = "SELECT * FROM `posts`";
+                connection.query(sql, function (err, result) {
+                    res.status(200);
+                    res.send(result);
+                });
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
+// Получение поста по ID
+app.get('/post', function (req, res) {
+    var sessionkey = req.query.sessionkey;
+    var ID = req.query.ID;
+    if (!(sessionkey && ID)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+        var sql = "SELECT * FROM `users` " + "WHERE sessionkey = '".concat(sessionkey, "'");
+        connection.query(sql, function (err, result) {
+            if (!(result.length == 0)) {
+                var sql = "SELECT * FROM `posts` WHERE `id` = " + "'".concat(ID, "'");
+                connection.query(sql, function (err, result) {
+                    res.status(200);
+                    res.send(result);
+                });
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
 // Удаление поста
-app.delete('/post', (req, res) => {
-    const sessionkey = req.query.sessionkey;
-    const ID = req.query.ID;
-    var sql = "SELECT * FROM `users` " + `WHERE id = '${ID}'`;
-    connection.query(sql, function (err, result) {
-        if (err) {
-            switch (err) {
-                default: {
-                    res.status(400);
-                    res.send(err);
-                }
-            }
-        } else {
-            if (!result.length == 0) {
-    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
-    connection.query(sql, function (err, result) {
-        if (err) {
-            switch (err) {
-                default: {
-                    res.status(400);
-                    res.send(err);
-                }
-            }
-        } else {
-            if (!result.length == 0) {
-            var sql = "DELETE FROM `posts` WHERE `posts`.`id` = " + `${ID}`
-
-        connection.query(sql, function (err) {
-            if (err) {
-                switch(err.errno) {
-                    default: {
-                        res.status(400);
-                        res.send(err);
+app["delete"]('/post', function (req, res) {
+    var sessionkey = req.query.sessionkey;
+    var ID = req.query.ID;
+    if (!(sessionkey && ID)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+        var sql = "SELECT * FROM `users` " + "WHERE id = '".concat(ID, "'");
+        connection.query(sql, function (err, result) {
+            if (!(result.length == 0)) {
+                var sql = "SELECT * FROM `users` " + "WHERE sessionkey = '".concat(sessionkey, "'");
+                connection.query(sql, function (err, result) {
+                    if (!(result.length == 0)) {
+                        var sql = "DELETE FROM `posts` WHERE `posts`.`id` = " + "".concat(ID);
+                        connection.query(sql, function (err) {
+                            res.status(200);
+                            res.send("\u041F\u043E\u0441\u0442 \u0443\u0434\u0430\u043B\u0435\u043D!");
+                        });
                     }
-                }
-            } else {
-                res.status(200);
-                res.send(`Пост удален!`);
+                });
             }
-        })
-    }}
-})
-} else {
-    res.send(200);
-    res.send(`Поста под ID: ${ID} не существует!`);
-    }}
-})
-})
-
-
-app.use((res) => {
-    res.status(404).type('text/plain')
-    res.send('404 ERROR')
-})
-
+            else {
+                res.send(200);
+                res.send("\u041F\u043E\u0441\u0442\u0430 \u043F\u043E\u0434 ID: ".concat(ID, " \u043D\u0435 \u0441\u0443\u0449\u0435\u0441\u0442\u0432\u0443\u0435\u0442!"));
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        res.send(error);
+    }
+});
+app.use(function (res) {
+    res.status(404).type('text/plain');
+    res.send('404 ERROR');
+});
 app.listen(port, host, function () {
-  console.log(`http://${host}:${port}`)
-})
-
-
+    console.log("http://".concat(host, ":").concat(port));
+});
---
+++ index.ts
@@ -0,0 +1,195 @@
+const { v4: uuidv4 } = require('uuid');
+const app = require('express')();
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
+const config = require('./config.json')
+const connection = mysql.createConnection({
+  host     : config.DB.host,
+  user     : config.DB.user,
+  password : config.DB.password,
+  database : config.DB.database
+});
+
+const host = '127.0.0.1';
+const port = 5000;
+
+connection.connect(function(err: any) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// Регистрация нового пользователя
+app.post('/user', (req: { query: { name: string; password: string; }; }, res: { status: (arg0: number) => void; send: (arg0: string) => void; }): void => {
+    try {
+    const username: string = req.query.name;
+    const password: string = req.query.password;
+    if (!(username && password)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    const sql: string = "INSERT INTO `users` (`id`, `name`, `password`) VALUES " + `(Null, '${username}', '${password}')`;
+    connection.query(sql, function (err: any) {
+        res.status(201); 
+        res.send(`Пользователь ${username} был зарегистрирован!`); 
+
+    })
+    } catch (error) {
+        res.status(500);
+        res.send(error);    
+    }
+})
+
+// Авторизация (создание сессии)
+app.post('/auth', (req: { query: { name: any; password: any; }; }, res: { status: (arg0: number) => void; send: (arg0: string) => void; }) => {
+    const username: string = req.query.name;
+    const password: string = req.query.password;
+    if (!(username && password)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+    var sql: string = "SELECT * FROM `users` " + `WHERE name = '${username}' AND password = '${password}'`;
+    connection.query(sql, function (err: { errno: any; }, result: string | any[]) {
+    if (result.length == 0) {
+        res.status(400);
+        res.send('Аккаунт/Пароль неверный');
+        return;
+    }
+    const sessionkey = uuidv4();
+    var sql = `UPDATE \`users\` SET \`sessionkey\` = '${sessionkey}' WHERE \`users\`.\`name\` = '${username}'`
+    connection.query(sql, function (err: any, result: any) {
+        res.status(200);
+        res.send(`Авторизовано! Ключ сессии: ${sessionkey}`); 
+    })
+    })
+    } catch (error) {
+        res.status(500);
+        res.send(error);    
+    }
+})
+
+// Написание поста
+app.post('/post', (req: { query: { sessionkey: any; content: any; }; }, res: { status: (arg0: number) => void; send: (arg0: string) => void; }) => {
+    const sessionkey: string = req.query.sessionkey;
+    const content: any = req.query.content;
+    if (!(sessionkey && content)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
+    connection.query(sql, function (err: any, result: string | any[]) {
+    if (!(result.length == 0)) {
+        var user: string = result[0].name;
+        var sql: string = "INSERT INTO `posts` (`id`, `author`, `content`) VALUES " + `(Null, '${user}', '${content}')`;
+        connection.query(sql, function (err: { errno: any; }) {
+            res.status(201); 
+            res.send(`Сообщение ${content} от пользователя ${user} отправлено!`); 
+            return;
+        
+        });
+    } else res.status(400); res.send('Неверный ключ сессии!');
+
+    })
+} catch (error) {
+        res.status(500);
+        res.send(error);    
+}
+})
+
+// Получение всех постов
+app.get('/posts', (req: { query: { sessionkey: any; ID: any; }; }, res: { status: (arg0: number) => void; send: (arg0: string) => void; }) => {
+    const sessionkey = req.query.sessionkey;
+    if (!(sessionkey)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
+    connection.query(sql, function (err: any, result: string | any[]) {
+        if (!(result.length == 0)) {
+        var sql = "SELECT * FROM `posts`"
+        connection.query(sql, function (err: { errno: any; }, result: any) {
+            res.status(200);
+            res.send(result);
+        })
+        }
+    })
+} catch (error) {
+    res.status(500);
+    res.send(error);    
+}
+})
+
+// Получение поста по ID
+app.get('/post', (req: { query: { sessionkey: any; ID: any; }; }, res: { status: (arg0: number) => void; send: (arg0: string) => void; }) => {
+    const sessionkey = req.query.sessionkey;
+    const ID = req.query.ID;
+    if (!(sessionkey && ID)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+    var sql = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
+    connection.query(sql, function (err: any, result: string | any[]) {
+        if (!(result.length == 0)) {
+        var sql = "SELECT * FROM `posts` WHERE `id` = " + `'${ID}'`
+        connection.query(sql, function (err: { errno: any; }, result: any) {
+            res.status(200);
+            res.send(result);
+        })
+        }
+    })
+} catch (error) {
+    res.status(500);
+    res.send(error);    
+}
+})
+
+// Удаление поста
+app.delete('/post', (req: { query: { sessionkey: any; ID: any; }; }, res: { status: (arg0: number) => void; send: (arg0: string | number) => void; }) => {
+    const sessionkey: string = req.query.sessionkey;
+    const ID: number = req.query.ID;
+    if (!(sessionkey && ID)) {
+        res.status(400);
+        res.send('Отсутствуют аргументы');
+        return;
+    }
+    try {
+    var sql: string = "SELECT * FROM `users` " + `WHERE id = '${ID}'`;
+    connection.query(sql, function (err: any, result: string | any[]) {
+        if (!(result.length == 0)) {
+    var sql: string = "SELECT * FROM `users` " + `WHERE sessionkey = '${sessionkey}'`;
+    connection.query(sql, function (err: any, result: string | any[]) {
+        if (!(result.length == 0)) {
+        var sql: string = "DELETE FROM `posts` WHERE `posts`.`id` = " + `${ID}`
+        connection.query(sql, function (err: { errno: any; }) {
+            res.status(200);
+            res.send(`Пост удален!`);
+        })
+        }   
+    })
+        } else {
+            res.send(200);
+            res.send(`Поста под ID: ${ID} не существует!`);
+        }
+    })  
+} catch (error) {
+    res.status(500);
+    res.send(error);    
+}
+})
+
+app.use((res) => {
+    res.status(404).type('text/plain')
+    res.send('404 ERROR')
+})
+
+app.listen(port, host, function () {
+  console.log(`http://${host}:${port}`)
+})
